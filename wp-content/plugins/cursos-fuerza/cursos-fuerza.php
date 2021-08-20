1cf26e2596aa415e2b764705a2fc3b02d0522e29
Wesley Silva <wesleysilvadb@hotmail.com>
2021-08-20 17:44:20 -0300

    Inicio da lógica de criação do plugin

--- wp-content/plugins/cursos-fuerza/cursos-fuerza.php
+++ wp-content/plugins/cursos-fuerza/cursos-fuerza.php
@@ -2,8 +2,33 @@
 
 /*
 Plugin Name: Cursos Fuerza
-Description: Sistema de gestão de cursos da Fuerza
+Description: Sistema de gestão de cursos da Fuerza Studio
 Author: Wesley Silva
 */
+
+function criaTabelaInscricoes() {
+ 
+	global $wpdb;
+
+	$charset = $wpdb->get_charset_collate();
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+	$nomeTabela = $wpdb->prefix . 'cursos_fuerza_inscricoes';
+
+	$sql = "CREATE TABLE {$nomeTabela} (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(150) NOT NULL,
+	email VARCHAR(150) NOT NULL,
+	id_curso INT(11) NOT NULL,
+	data_inscricao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+	) $charset;";
+	
+	dbDelta( $sql );
+	
+}
+
+register_activation_hook( __FILE__, 'criaTabelaInscricoes' );
 
 require_once plugin_dir_path(__FILE__) . 'includes/cf-funcoes.php';
--- wp-content/plugins/cursos-fuerza/includes/cf-funcoes.php
+++ wp-content/plugins/cursos-fuerza/includes/cf-funcoes.php
@@ -20,52 +20,257 @@ add_action('init', 'cfAdicionaLinkAdministrativo');
 
 
 function registraCampoAdicional() {
-    add_meta_box( 'cf-box-info-adicionais', esc_html__( 'Informações adicionais', 'cf_dominio' ), 'cfInfoAdicionais', 'cursos-fuerza' );
-	//add_meta_box( 'cf-box-info-adicionais-2', esc_html__( 'Informações adicionais', 'cf_dominio' ), 'cfInfoAdicionais2', 'cursos-fuerza' );
+    add_meta_box( 'cf_box_link_inscricao', esc_html__( 'Link de inscrição para o curso', 'cf_dominio' ), 'cfCampoLinkInscricao', 'cursos-fuerza' );
+	add_meta_box( 'cf_box_carga_horaria', esc_html__( 'Carga horária do curso', 'cf_dominio' ), 'cfCampoCargaHoraria', 'cursos-fuerza' );
+	add_meta_box( 'cf_box_data_limite_inscricoes', esc_html__( 'Data limite de inscrições para o curso', 'cf_dominio' ), 'cfCampoDataLimiteInscricoes', 'cursos-fuerza' );
 }
 
 add_action( 'add_meta_boxes', 'registraCampoAdicional');
  
 
-function cfInfoAdicionais( $meta_id ) {
+function cfCampoLinkInscricao( $meta_id ) {
 
     $outline = '<label for="cf_link_inscricao" style="width:150px; display:inline-block;">'. esc_html__('Link de inscrição', 'cf_dominio') .'</label>';
     $title_field = get_post_meta( $meta_id->ID, 'cf_link_inscricao', true );
-	$outline .= '<input type="text" name="cf_link_inscricao" id="cf_link_inscricao" value="'. esc_attr($title_field) .'" style="width:300px;"/>';
+	$outline .= '<input type="url" placeholder="https://www.fuerzastudio.com.br/cursos/php" name="cf_link_inscricao" id="cf_link_inscricao" value="'. esc_attr($title_field) .'" style="width:300px;" required/>';
     echo $outline;
 }
 
-function cfInfoAdicionais2( $meta_id ) {
+function cfCampoCargaHoraria( $meta_id ) {
  
-    $outline = '<label for="cf_link_inscricao" style="width:150px; display:inline-block;">'. esc_html__('Link de inscrição', 'cf_dominio') .'</label>';
-    $title_field = get_post_meta( $meta_id->ID, 'cf_link_inscricao', true );
-    $outline .= '<input type="text" name="cf_link_inscricao" id="cf_link_inscricao" value="'. esc_attr($title_field) .'" style="width:300px;"/>';
+    $outline = '<label for="cf_carga_horaria" style="width:150px; display:inline-block;">'. esc_html__('Carga horária em horas', 'cf_dominio') .'</label>';
+    $title_field = get_post_meta( $meta_id->ID, 'cf_carga_horaria', true );
+    $outline .= '<input type="number" name="cf_carga_horaria" id="cf_carga_horaria" value="'. esc_attr($title_field) .'" style="width:300px;" required/>';
  
     echo $outline;
 }
 
+function cfCampoDataLimiteInscricoes( $meta_id ) {
+ 
+    $outline = '<label for="cf_data_limite_inscricoes" style="width:150px; display:inline-block;">'. esc_html__('Data limite de inscrições', 'cf_dominio') .'</label>';
+    $title_field = get_post_meta( $meta_id->ID, 'cf_data_limite_inscricoes', true );
+    $outline .= '<input type="date" name="cf_data_limite_inscricoes" id="cf_data_limite_inscricoes" value="'. esc_attr($title_field) .'" style="width:300px;" required/>';
+ 
+    echo $outline;
+}
 
-/* Faça algo com os dados inseridos */
 add_action( 'save_post', 'cfSalvaDadosPost' );
 
 /* Quando o post for salvo, salvamos também nossos dados personalizados */
-function cfSalvaDadosPost( $post_id ) {
+function cfSalvaDadosPost( int $idPost ) {
 
-  // É necessário verificar se o usuário está autorizado a fazer isso
   if ( 'cursos-fuerza' == $_POST['post_type'] ) {
-    if ( ! current_user_can( 'edit_page', $post_id ) )
+    if ( ! current_user_can( 'edit_page', $idPost ) )
         return;
   } else {
-    if ( ! current_user_can( 'edit_post', $post_id ) )
+    if ( ! current_user_can( 'edit_post', $idPost ) )
         return;
   }
+  
+  $camposPersonalizados = [
+	'cf_link_inscricao' => sanitize_text_field( $_POST['cf_link_inscricao'] ),
+	'cf_carga_horaria' => sanitize_text_field( $_POST['cf_carga_horaria'] ),
+	'cf_data_limite_inscricoes' => sanitize_text_field( $_POST['cf_data_limite_inscricoes'] ),
+  ];
 
-  // Recebe o ID do post
-  $post_ID = $_POST['post_ID'];
+  foreach ($camposPersonalizados as $nomeCampo => $valorCampo) {
+	  
+	  // Adiciona ou atualiza os campos
+	  update_post_meta($idPost, $nomeCampo, $valorCampo);
+	  
+  }
 
-  // Remove caracteres indesejados
-  $mydata = sanitize_text_field( $_POST['cf_link_inscricao'] );
+}
 
-  // Adicionamos ou atualizados o $mydata 
-    update_post_meta($post_ID, 'cf_link_inscricao', $mydata);
+add_filter('single_template', function($original){
+	
+	global $post;
+
+	$tipoPost = $post->post_type;
+
+	if (trim(strtolower($tipoPost)) == 'cursos-fuerza') {
+	  
+		add_filter( 'the_content', 'adicionaInformacoesPersonalizadas' );  
+	}
+	
+	return $original;
+  
+});
+
+function adicionaInformacoesPersonalizadas( $conteudo ) {
+	
+	$cargaHoraria = get_post_meta(get_the_ID(), 'cf_carga_horaria', true);
+	
+	$dataLimiteInscricao = new DateTime(get_post_meta(get_the_ID(), 'cf_data_limite_inscricoes', true));
+	
+	$idCurso = base64_encode(get_the_ID());
+	
+	$linkInscricao = get_post_meta(get_the_ID(), 'cf_link_inscricao', true);
+	
+	$linkInscricaoCodificado = base64_encode($linkInscricao);
+	
+	$dataLimiteFormatada = $dataLimiteInscricao->format('d/m/Y');
+	
+	$formInscricao = "<p><a href='{$linkInscricao}' target='_blank' title='Efetuar inscrição' class='btn btn-sucesso'>Efetuar inscrição</a></p>";
+	
+	if (strtotime(date('Y-m-d')) <= strtotime($dataLimiteInscricao->format('Y-m-d'))) {
+		
+		$formInscricao = <<<FORM_INSCRICAO
+			<h4>Tenho Interesse</h4>
+			<form method="post" id="formInscreveCurso">
+			  <p class="margin-baixo-20">
+				<label for="nome">Nome:</label>
+				<input type="text" id="nome" name="nome" class="input-100" required>
+			  </p>
+			  <p class="margin-baixo-20">
+				<label for="email">E-mail:</label>
+				<input type="email" id="email" name="email" class="input-100" required>
+			  </p>
+			  <p class="margin-baixo-20">		  
+				<input type="submit" value="Enviar">
+			  </p>
+			  <input type="hidden" value="{$idCurso}" id="curso">
+			  <input type="hidden" value="{$linkInscricaoCodificado}" id="url">
+			</form>
+			<div class="mensagem-inscricao sucesso"></div>
+FORM_INSCRICAO;
+		
+	}
+	
+	$textoCustomizado = <<<INFORMACOES_EXTRAS
+		<h4>Detalhes do curso</h4>
+		<p class="font-tamanho-16 margin-topo-0 margin-baixo-0">Carga Horária: <strong>{$cargaHoraria}</strong> horas</p>
+		<p class="font-tamanho-16 margin-topo-0 margin-baixo-0">Data limite para inscrições: <strong>{$dataLimiteFormatada}</strong></p>
+		{$conteudo}
+		{$formInscricao}
+INFORMACOES_EXTRAS;
+ 
+    return $textoCustomizado;
+}
+
+function adicionaAssetsFront() {
+    
+	wp_enqueue_style('cf-estilo', plugins_url( 'layout/assets/css/estilos.css', __FILE__ ), array(), false);
+	wp_enqueue_script('jquery', plugins_url( 'layout/assets/js/jquery.min.js', __FILE__ ), array(), false);
+	wp_enqueue_script('cf-script', plugins_url( 'layout/assets/js/script.js', __FILE__ ), array(), false);
+
+}
+
+add_action( 'wp_enqueue_scripts', 'adicionaAssetsFront' );
+
+add_action('wp_footer', 'adicionarScriptRodape');
+
+function adicionarScriptRodape() {
+    ?>
+        <script>
+            const URL = '<?php echo get_site_url(); ?>';
+        </script>
+    <?php
+}
+
+add_action('rest_api_init', function() {
+	
+  register_rest_route( 'cursos-fuerza/v1', '/inscricao', [
+    'methods' => 'POST',
+    'callback' => 'apiInscricaoCurso',
+  ]);
+  
+});
+
+function apiInscricaoCurso( WP_REST_Request $requisicao ) {
+	
+	$nome = sanitize_text_field($requisicao['nome']);
+	
+	$email = sanitize_text_field($requisicao['email']);
+	
+	$idCurso = sanitize_text_field(base64_decode($requisicao['idCurso']));
+	
+	if (empty($nome) || empty($email) || empty($idCurso)) {
+		
+		return rest_ensure_response(['erro' => true, 'mensagem' => 'Requisição inválida.']);
+		
+	}
+	
+	$salvaInscricao = salvaInscricaoNoBanco($nome, $email, $idCurso);
+	
+	if ($salvaInscricao[0]) {
+	
+		return rest_ensure_response(['erro' => false, 'mensagem' => $salvaInscricao[1]]);
+	}
+	
+	return rest_ensure_response(['erro' => true, 'mensagem' => $salvaInscricao[1]]);
+	
+}
+
+function salvaInscricaoNoBanco($nome, $email, $idCurso) {
+	
+	global $wpdb;
+	
+	$tabela = $wpdb->prefix . 'cursos_fuerza_inscricoes';
+	
+	$usuarioJaCadastrado = $wpdb->get_results( $wpdb->prepare("SELECT * FROM {$tabela} WHERE email = %s AND id_curso = %d", $email, $idCurso) );
+	
+	if ($usuarioJaCadastrado) {
+		
+		return [false, 'Você já está cadastrado para este curso'];
+		
+	}
+	
+	$dados = [
+		'nome' => $nome,
+		'email' => $email,
+		'id_curso' => $idCurso
+	];
+	
+	$formato = [
+		'%s',
+		'%s',
+		'%d'
+	];
+	
+	$inseriu = $wpdb->insert( $tabela, $dados, $formato );
+	
+	if($inseriu){
+		
+		return [true, 'Inscrição realizada com sucesso.'];
+		
+	}
+	
+	return [false, 'Falha ao realizar inscrição.'];
+
+}
+
+function add_admin_column($column_title, $post_type, $cb){
+
+    // Column Header
+    add_filter( 'manage_' . $post_type . '_posts_columns', function($columns) use ($column_title) {
+        $columns[ sanitize_title($column_title) ] = $column_title;
+        return $columns;
+    } );
+
+    // Column Content
+    add_action( 'manage_' . $post_type . '_posts_custom_column' , function( $column, $post_id ) use ($column_title, $cb) {
+
+        if(sanitize_title($column_title) === $column){
+            $cb($post_id);
+        }
+
+    }, 10, 2 );
+}
+
+add_admin_column(__('Usuários interessados'), 'cursos-fuerza', function($idPost){
+    
+	echo recuperaUsuariosPorCurso($idPost);
+	
+});
+
+function recuperaUsuariosPorCurso(int $idCurso) {
+	
+	global $wpdb;
+	
+	$tabela = $wpdb->prefix . 'cursos_fuerza_inscricoes';
+	
+	$qtdInteressados = $wpdb->get_row( $wpdb->prepare("SELECT COUNT(id_curso) AS qtdInteressados FROM {$tabela} WHERE id_curso = %d", $idCurso) );
+	
+	return $qtdInteressados->qtdInteressados;
 }
