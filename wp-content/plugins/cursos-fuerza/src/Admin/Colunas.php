059eea82df8f524a1d56b6b9729825f30b27ada1
Wesley Silva <wesleysilvadb@hotmail.com>
2021-08-23 17:27:15 -0300

    Classe que cria as colunas do admin

---
+++ wp-content/plugins/cursos-fuerza/src/Admin/Colunas.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace Fuerza\Admin;
+
+class Colunas
+{
+    
+    /**
+     * Method adicionar
+     *
+     * @param $tituloColuna $tituloColuna Título da coluna na listagem de posts
+     * @param $tipoPost $tipoPost Tipo de post
+     * @param $callback $callback Função de callback
+     *
+     * @return void
+     */
+    public function adicionar($tituloColuna, $tipoPost, $callback) : void
+    {
+        
+        add_filter('manage_' . $tipoPost . '_posts_columns', function($colunas) use ($tituloColuna) {
+            
+            unset($colunas['date']);
+
+            $colunas[sanitize_title($tituloColuna)] = $tituloColuna;
+
+            $colunas['date'] = __('Date');
+
+            return $colunas;
+
+        });
+
+        add_action('manage_' . $tipoPost . '_posts_custom_column' , function($coluna, $idPost) use ($tituloColuna, $callback) {
+
+            if (sanitize_title($tituloColuna) === $coluna) {
+
+                $callback($idPost);
+
+            }
+
+        }, 10, 2);
+
+    }
+
+}
