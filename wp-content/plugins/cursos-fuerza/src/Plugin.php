936fdeb6efd3b85380ba668c10049f286afacfc6
Wesley Silva <wesleysilvadb@hotmail.com>
2021-08-24 15:04:55 -0300

    Passando o tipo de post para constante

--- wp-content/plugins/cursos-fuerza/src/Plugin.php
+++ wp-content/plugins/cursos-fuerza/src/Plugin.php
@@ -27,7 +27,7 @@ class Plugin
 
             new InstalaPlugin();
 
-            $dadosPostPersonalizado = new PostPersonalizadoDados('cursos-fuerza', 'Cursos Fuerza', 'Curso Fuerza', true, false, ['title', 'editor', 'thumbnail', 'custom-fields']);
+            $dadosPostPersonalizado = new PostPersonalizadoDados(TIPO_POST, 'Cursos Fuerza', 'Curso Fuerza', true, false, ['title', 'editor', 'thumbnail', 'custom-fields']);
             
             $postPersonalizado = new PostPersonalizado($dadosPostPersonalizado);
         
@@ -36,9 +36,9 @@ class Plugin
             $campoPersonalizado = new CampoAdicional(new TemplateCampoAdicional);
 
             $campoPersonalizado
-                ->defineDadosCampoAdicional('cf_box_link_inscricao', 'Link de inscrição para o curso', 'campoLinkInscricao', 'cursos-fuerza')
-                ->defineDadosCampoAdicional('cf_box_carga_horaria', 'Carga horária do curso', 'campoCargaHoraria', 'cursos-fuerza')
-                ->defineDadosCampoAdicional('cf_box_data_limite_inscricoes', 'Data limite de inscrições para o curso', 'campoDataLimiteInscricoes', 'cursos-fuerza')
+                ->defineDadosCampoAdicional('cf_box_link_inscricao', 'Link de inscrição para o curso', 'campoLinkInscricao', TIPO_POST)
+                ->defineDadosCampoAdicional('cf_box_carga_horaria', 'Carga horária do curso', 'campoCargaHoraria', TIPO_POST)
+                ->defineDadosCampoAdicional('cf_box_data_limite_inscricoes', 'Data limite de inscrições para o curso', 'campoDataLimiteInscricoes', TIPO_POST)
                 ->executaCriacaoCampos();
 
             $dadosAdicionais = new PostSalvaDadoAdicional();
@@ -68,7 +68,7 @@ class Plugin
 
             $colunaInteressados = new ColunaUsuariosInteressados(new Colunas, $inscricao);
 
-            $colunaInteressados->adiciona('Usuários interessados', 'cursos-fuerza');
+            $colunaInteressados->adiciona('Usuários interessados', TIPO_POST);
 
             $inscricao->executaListaInteressados();
         
